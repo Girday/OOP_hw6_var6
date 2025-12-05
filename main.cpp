e48b3eb5f5a0cc8c05a6acc927996bee6805326d
Maxim Grigorev <123688667+Girday@users.noreply.github.com>
2025-12-05 18:35:01 +0300

    update main

--- main.cpp
+++ main.cpp
@@ -66,9 +66,8 @@ std::shared_ptr<NPC> factory(std::istream& is) {
             result = std::make_shared<Bandit>(is);
             break;
         }
-    } else {
+    } else
         std::cerr << "Unexpected NPC type: " << type << std::endl;
-    }
 
     if (result) {
         result->subscribe(TextObserver::get());
@@ -93,6 +92,7 @@ std::shared_ptr<NPC> factory(NpcType type, const std::string& name, int x, int y
     default:
         break;
     }
+
     if (result) {
         result->subscribe(TextObserver::get());
         result->subscribe(FileObserver::get());
@@ -104,8 +104,10 @@ std::shared_ptr<NPC> factory(NpcType type, const std::string& name, int x, int y
 void save(const set_t& array, const std::string& filename) {
     std::ofstream fs(filename);
     fs << array.size() << std::endl;
+    
     for (auto& n : array)
         n->save(fs);
+
     fs.flush();
     fs.close();
 }
@@ -113,21 +115,23 @@ void save(const set_t& array, const std::string& filename) {
 set_t load(const std::string& filename) {
     set_t result;
     std::ifstream is(filename);
+    
     if (is.good() && is.is_open()) {
         int count;
         is >> count;
         for (int i = 0; i < count; ++i)
             result.insert(factory(is));
         is.close();
-    } else {
+    } else
         std::cerr << "Error: " << std::strerror(errno) << std::endl;
-    }
+
     return result;
 }
 
 std::ostream& operator<<(std::ostream& os, const set_t& array) {
     for (auto& n : array)
         n->print(os);
+    
     return os;
 }
 
@@ -153,16 +157,17 @@ int main() {
         NpcType type = static_cast<NpcType>(std::rand() % 3 + 1);
         std::string name;
         switch (type) {
-        case BearType: name = "bear_"; break;
-        case ElfType: name = "elf_"; break;
-        case BanditType: name = "bandit_"; break;
-        case Unknown: name = "unknown_"; break;
+            case BearType: name = "bear_"; break;
+            case ElfType: name = "elf_"; break;
+            case BanditType: name = "bandit_"; break;
+            case Unknown: name = "unknown_"; break;
         }
+
         name += std::to_string(i);
         array.insert(factory(type, name, std::rand() % 501, std::rand() % 501));
     }
+    
     std::cout << "Saving ..." << std::endl;
-
     save(array, "npc.txt");
 
     std::cout << "Loading ..." << std::endl;
@@ -174,6 +179,7 @@ int main() {
         auto dead_list = fight(array, distance);
         for (auto& d : dead_list)
             array.erase(d);
+            
         std::cout << "Fight stats ----------" << std::endl
                   << "distance: " << distance << std::endl
                   << "killed: " << dead_list.size() << std::endl
